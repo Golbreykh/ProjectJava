ca787a0f0cb4289d830d9b0dee221ef7d68556cf
Golbreykh <82124976+Golbreykh@users.noreply.github.com>
2022-01-31 15:31:55 +0300

    Add files via upload
    Проект на JavaFX

---
+++ JavaFXApp/src/javafxapp/JavaFXApp.java
@@ -0,0 +1,25 @@
+package javafxapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+public class JavaFXApp extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GridPane.fxml"));
+        Scene scene = new Scene(root);
+        
+        //Scene scene = new Scene(createPane(), 550, 550);
+        stage.setTitle("Concurrency in JavaFX");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
